c8125900b98b5dbb9241f6522dfed756cb22889a
Anthony Le Goas <anthony.legoas@gmail.com>
2023-02-25 18:01:57 +0100

    ✨ add first version of campaigns page

---
+++ src/routes/campaigns/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Row, Section } from '@smui/top-app-bar';
+	import Button, { Label } from '@smui/button';
+
+	import Switch from '@smui/switch';
+	import FormField from '@smui/form-field';
+
+	let checked1 = true;
+	let checked2 = false;
+</script>
+
+<Row>
+	<Section>
+		<h1>My campaigns</h1>
+	</Section>
+	<Section align="end" toolbar>
+		<Button
+			on:click={() => null}
+			color="secondary"
+			variant="unelevated"
+			class="button-shaped-round"
+		>
+			<Label>New campaign</Label>
+		</Button>
+	</Section>
+</Row>
+<Section>
+	<Row>
+		<FormField>
+			<Switch bind:checked={checked1} />
+			<span slot="label">Show expired campaiges</span>
+		</FormField>
+		<FormField>
+			<Switch bind:checked={checked2} />
+			<span slot="label">Show archived campaigns</span>
+		</FormField>
+	</Row>
+</Section>
