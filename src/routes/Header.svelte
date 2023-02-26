97259846c9a2193ba77d5f0c6d0a062a1ca3117c
Anthony Le Goas <anthony.legoas@gmail.com>
2023-02-26 16:24:08 +0100

    💄 ajust padding in header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -9,31 +9,42 @@
 	const logIn = () => (loggedIn = true);
 </script>
 
-<Row>
-	<Section>
-		<Title class="title">TUX</Title>
-		<Logo />
-	</Section>
-	<Section align="end" toolbar>
-		<div class="space-around">
-			{#if loggedIn}
-				<Button on:click={() => null} variant="unelevated" class="button-shaped-round">
-					<Icon class="material-icons">account_circle</Icon>
-					<Label>Karine</Label>
-				</Button>
-			{:else}
-				<Button on:click={logIn} class="button-shaped-round">
-					<Label>Log in</Label>
-				</Button>
-				<Button color="secondary" on:click={logIn} variant="unelevated" class="button-shaped-round">
-					<Label>Sign up</Label>
-				</Button>
-			{/if}
-		</div>
-	</Section>
-</Row>
+<header>
+	<Row>
+		<Section>
+			<Title class="title">TUX</Title>
+			<Logo />
+		</Section>
+		<Section align="end" toolbar>
+			<div class="space-around">
+				{#if loggedIn}
+					<Button on:click={() => null} variant="unelevated" class="button-shaped-round">
+						<Icon class="material-icons">account_circle</Icon>
+						<Label>Karine</Label>
+					</Button>
+				{:else}
+					<Button on:click={logIn} class="button-shaped-round">
+						<Label>Log in</Label>
+					</Button>
+					<Button
+						color="secondary"
+						on:click={logIn}
+						variant="unelevated"
+						class="button-shaped-round"
+					>
+						<Label>Sign up</Label>
+					</Button>
+				{/if}
+			</div>
+		</Section>
+	</Row>
+</header>
 
 <style>
+	header {
+		padding: 0.25rem 1rem;
+	}
+
 	:global(.space-around > button) {
 		margin: 0 0.25rem;
 	}
