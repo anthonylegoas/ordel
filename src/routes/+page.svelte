07fee815940e0778c557265d3ba5b5ee836e0955
Anthony Le Goas <anthony.legoas@gmail.com>
2023-02-28 23:24:02 +0100

    ✨ add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Button, { Label } from '@smui/button';
+	import { Banner } from '$lib/cdk';
+	import { OrdelInstructions } from '$lib/components';
 </script>
 
 <svelte:head>
@@ -8,17 +9,29 @@
 </svelte:head>
 
 <section>
-	<p>Home page</p>
-	<Button on:click={() => null}>
-		<Label>Default</Label>
-	</Button>
-	<Button on:click={() => null} color="secondary">
-		<Label>Default</Label>
-	</Button>
+	<h1>Nouvel Ordel Mondial</h1>
+	<h2>Avec Ordel, ordonner une liste d'objets n'a jamais été aussi simple.</h2>
+	<Banner />
+	<OrdelInstructions />
 </section>
 
 <style>
 	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		padding: 4rem;
+		padding: 24px 0px;
+		gap: 20px;
+	}
+
+	h1 {
+		font-family: 'Source Serif Pro';
+		font-style: normal;
+		font-weight: 900;
+		font-size: 57px;
+		line-height: 64px;
+		margin: 0;
 	}
 </style>
