57805df3f92bb2a7c207ad87debdcb54cfd32aa3
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-02 12:38:14 +0100

    ✨ add <Link> to cdk

---
+++ src/lib/cdk/Link/Link.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a {href} class="button-shaped-round mdc-button mdc-top-app-bar__action-item mdc-ripple-upgraded">
+	<div class="mdc-button__ripple" />
+	<span class="mdc-button__label"> <slot /></span></a
+>
