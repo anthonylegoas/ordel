369d69f8b774a86424fd8a08c24841c955cb1e03
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-02 17:12:37 +0100

    💫 animate <Banner>

--- src/lib/cdk/Banner/Banner.svelte
+++ src/lib/cdk/Banner/Banner.svelte
@@ -1,69 +1,137 @@
+<script lang="ts">
+	type CursorPosition = {
+		x: number;
+		y: number;
+	};
+	let cursorPosition: CursorPosition = { x: 0, y: 0 };
+	// $: xBase = cursorPosition.x / 4 - cursorPosition.x / 6;
+	const transitionDurationBase = 3;
+
+	const mousemoveHandler = (event: MouseEvent) => {
+		cursorPosition = { x: event.x, y: event.y };
+	};
+</script>
+
+<svelte:window on:mousemove={mousemoveHandler} />
 <svg
-	width="1237"
+	width="100vw"
 	height="253"
 	viewBox="0 0 1237 253"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
 >
 	<g clip-path="url(#clip0_192_5062)">
-		<path d="M425 133L450.981 178H399.019L425 133Z" fill="#6750A4" />
+		<!-- <path
+			style={`transform:  translate( ${xBase}px,0px); transition-duration: ${
+				transitionDurationBase - 0.08
+			}s;`}
+			d="M425 133L450.981 178H399.019L425 133Z"
+			fill="#6750A4"
+		/> -->
 		<path
+			style={`animation-duration: ${transitionDurationBase - 0.08}s;`}
+			d="M425 133L450.981 178H399.019L425 133Z"
+			fill="#6750A4"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.1}s;`}
 			d="M212.912 91.851L211.015 85.639L212.449 85.2009L212.259 84.5797L212.892 84.726L213.23 83.2645L219.558 84.7277L219.22 86.1891L225.549 87.6523L225.887 86.1909L232.215 87.654L231.877 89.1155L238.205 90.5786L238.543 89.1172L244.871 90.5803L244.533 92.0418L250.862 93.5049L251.2 92.0435L257.528 93.5066L257.19 94.9681L257.823 95.1144L257.38 95.5893L258.476 96.6126L254.045 101.361L252.948 100.338L248.517 105.087L249.614 106.11L245.183 110.859L244.086 109.836L239.655 114.585L240.751 115.608L236.32 120.357L235.223 119.333L230.792 124.082L231.889 125.106L227.458 129.855L226.361 128.831L225.918 129.306L225.728 128.685L224.293 129.123L222.396 122.911L223.831 122.473L221.934 116.261L220.499 116.699L218.602 110.487L220.037 110.049L218.14 103.837L216.705 104.275L214.808 98.063L216.243 97.6249L214.346 91.4129L212.912 91.851Z"
 			stroke="#6750A4"
 			stroke-width="3"
 			stroke-dasharray="6 6"
 		/>
-		<path d="M553.392 58.0456L592.514 92.2431L543.337 109.025L553.392 58.0456Z" fill="#D0BCFF" />
-		<path d="M345.29 39.1479L400.136 92.0622L326.888 113.103L345.29 39.1479Z" fill="white" />
-		<path d="M851.315 22.2455L921.268 52.4893L860.1 97.9478L851.315 22.2455Z" fill="#D0BCFF" />
-		<path d="M959.967 58.7027L1087.41 31.0567L1047.63 155.25L959.967 58.7027Z" fill="white" />
 		<path
+			style={`animation-duration: ${transitionDurationBase - 0.06}s;`}
+			d="M553.392 58.0456L592.514 92.2431L543.337 109.025L553.392 58.0456Z"
+			fill="#D0BCFF"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.1}s;`}
+			d="M345.29 39.1479L400.136 92.0622L326.888 113.103L345.29 39.1479Z"
+			fill="white"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase + 0.1}s;`}
+			d="M851.315 22.2455L921.268 52.4893L860.1 97.9478L851.315 22.2455Z"
+			fill="#D0BCFF"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.07}s;`}
+			d="M959.967 58.7027L1087.41 31.0567L1047.63 155.25L959.967 58.7027Z"
+			fill="white"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.1}s;`}
 			d="M592.135 181.319L595.344 125.986L641.659 156.432L592.135 181.319Z"
 			stroke="#CCC2DC"
 			stroke-width="3"
 		/>
-		<path d="M753.273 42.7159L803.93 76.0153L749.764 103.236L753.273 42.7159Z" fill="#E8DEF8" />
 		<path
+			style={`animation-duration: ${transitionDurationBase}s;`}
+			d="M753.273 42.7159L803.93 76.0153L749.764 103.236L753.273 42.7159Z"
+			fill="#E8DEF8"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.1}s;`}
 			d="M513.242 156.756L530.77 145.586L531.679 166.35L513.242 156.756Z"
 			stroke="#6750A4"
 			stroke-width="3"
 		/>
 		<path
+			style={`animation-duration: ${transitionDurationBase + 0.1}s;`}
 			d="M299.214 162.839L308.516 144.252L319.962 161.601L299.214 162.839Z"
 			stroke="white"
 			stroke-width="3"
 		/>
-		<path d="M923.973 59.8937L938.281 81.5797L912.347 83.1278L923.973 59.8937Z" fill="#CCC2DC" />
-		<path d="M466.317 9.84521L490.885 18.2943L471.284 35.3467L466.317 9.84521Z" fill="white" />
 		<path
+			style={`anumation-duration: ${transitionDurationBase + 0.05}s;`}
+			d="M923.973 59.8937L938.281 81.5797L912.347 83.1278L923.973 59.8937Z"
+			fill="#CCC2DC"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase + 0.08}s;`}
+			d="M466.317 9.84521L490.885 18.2943L471.284 35.3467L466.317 9.84521Z"
+			fill="white"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.05}s;`}
 			d="M606.795 112.191L614.792 83.8531L635.335 104.948L606.795 112.191Z"
 			stroke="#E8DEF8"
 			stroke-width="3"
 		/>
 		<path
+			style={`animation-duration: ${transitionDurationBase - 0.02}s;`}
 			d="M420.032 39.962L424.261 35.9972L425.287 37.0915L425.506 36.8861L425.574 37.1786L427.035 36.8373L428.354 42.4824L426.893 42.8236L428.212 48.4687L429.673 48.1274L430.992 53.7724L429.531 54.1137L430.85 59.7587L432.311 59.4174L433.63 65.0624L432.169 65.4037L433.488 71.0487L434.949 70.7074L436.268 76.3525L434.807 76.6937L436.126 82.3388L437.587 81.9975L438.905 87.6425L437.445 87.9838L437.513 88.2763L437.226 88.1893L436.791 89.6249L431.243 87.9446L431.677 86.509L426.129 84.8287L425.694 86.2643L420.146 84.5841L420.581 83.1485L415.033 81.4682L414.598 82.9038L409.05 81.2236L409.485 79.7879L403.936 78.1077L403.502 79.5433L397.953 77.863L398.388 76.4274L392.84 74.7472L392.405 76.1828L386.857 74.5025L387.292 73.0669L387.004 72.9798L387.223 72.7743L386.198 71.68L390.427 67.7152L391.453 68.8096L395.682 64.8448L394.656 63.7505L398.885 59.7857L399.911 60.8801L404.14 56.9153L403.115 55.821L407.344 51.8562L408.37 52.9506L412.599 48.9858L411.573 47.8915L415.802 43.9267L416.828 45.0211L421.057 41.0563L420.032 39.962Z"
 			stroke="#6750A4"
 			stroke-width="3"
 			stroke-dasharray="6 6"
 		/>
 		<path
+			style={`animation-duration: ${transitionDurationBase + 0.03}s;`}
 			d="M603.045 210.268L621.797 187.567L632.081 215.157L603.045 210.268Z"
 			stroke="#6750A4"
 			stroke-width="3"
 		/>
-		<path d="M717.434 107.847L726.508 132.192L700.888 127.878L717.434 107.847Z" fill="#6750A4" />
 		<path
+			style={`animation-duration: ${transitionDurationBase + 0.1}s;`}
+			d="M717.434 107.847L726.508 132.192L700.888 127.878L717.434 107.847Z"
+			fill="#6750A4"
+		/>
+		<path
+			style={`animation-duration: ${transitionDurationBase - 0.012}s;`}
 			d="M716.512 167.74L742.435 136.359L756.65 174.499L716.512 167.74Z"
 			stroke="#6750A4"
 			stroke-width="3"
 		/>
 		<path
+			style={`animation-duration: ${transitionDurationBase + 0.05}s;`}
 			d="M153.512 65.7399L179.435 34.3587L193.65 72.4987L153.512 65.7399Z"
 			fill="#625B71"
 			stroke="#6750A4"
 			stroke-width="3"
 		/>
 		<path
+			style={`animation-duration: ${transitionDurationBase + 0.15}s;`}
 			d="M916.041 123.09L919.681 118.683L920.838 119.638L921.003 119.438L921.094 119.681L922.499 119.158L924.496 124.513L923.09 125.037L925.086 130.393L926.492 129.869L928.488 135.225L927.083 135.749L929.079 141.105L930.484 140.581L932.481 145.937L931.075 146.461L933.071 151.816L934.477 151.293L936.473 156.648L935.068 157.172L937.064 162.528L938.469 162.004L940.466 167.36L939.06 167.884L939.151 168.127L938.895 168.084L938.646 169.563L933.009 168.614L933.258 167.135L927.622 166.186L927.373 167.665L921.736 166.716L921.985 165.237L916.349 164.288L916.1 165.767L910.463 164.818L910.713 163.339L905.076 162.39L904.827 163.869L899.191 162.92L899.44 161.441L893.803 160.491L893.554 161.971L887.918 161.021L888.167 159.542L887.911 159.499L888.076 159.299L886.92 158.344L890.56 153.937L891.716 154.892L895.356 150.485L894.2 149.53L897.84 145.123L898.997 146.079L902.637 141.672L901.48 140.717L905.12 136.31L906.277 137.265L909.917 132.858L908.761 131.903L912.401 127.496L913.557 128.452L917.197 124.045L916.041 123.09Z"
 			fill="#E8DEF8"
 			stroke="#CCC2DC"
@@ -77,3 +145,24 @@
 		</clipPath>
 	</defs>
 </svg>
+
+<style>
+	path {
+		transition: transform ease-out;
+		transition-delay: 0;
+		transform-origin: 50% 50%;
+		animation: float 3s ease-in-out infinite;
+	}
+
+	@keyframes float {
+		0% {
+			transform: translatey(0px);
+		}
+		50% {
+			transform: translatey(-10px);
+		}
+		100% {
+			transform: translatey(0px);
+		}
+	}
+</style>
