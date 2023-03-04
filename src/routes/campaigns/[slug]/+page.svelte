b01dcc59623586d8e8f568c22deb8a16520987c5
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-04 23:31:14 +0100

    ✨ add first step of campaign voting funnel

--- src/routes/campaigns/[slug]/+page.svelte
+++ src/routes/campaigns/[slug]/+page.svelte
@@ -1,11 +1,54 @@
 <script lang="ts">
+	import Button, { Label } from '@smui/button';
 	import type { PageData } from '../$types';
+	import { campaignsStore } from '$lib/store';
+	import { Banner } from '$lib/cdk';
 
-	export let data: PageData;
+	type CampaignPageData = { campaignId: string } & PageData;
 
-	$: str = JSON.stringify(data);
-	console.log(data);
+	export let data: CampaignPageData;
+	const { campaigns } = campaignsStore;
+	const campaign = data.campaignId ? $campaigns.find((v) => v.id === data.campaignId) : null;
 </script>
 
-<h1>{data.title}</h1>
-<div>{@html str}</div>
+<section>
+	{#if campaign}
+		<h1 class="name">You are invited to vote on <b>{campaign.name}</b></h1>
+		<p class="description">{campaign.description}</p>
+		<div>
+			<Banner />
+			<Button on:click={() => null} variant="unelevated" class="button-shaped-round">
+				<Label>Start voting</Label>
+			</Button>
+			<p>Click on the proposal you wish to retain (or use its keyboard shortcut)</p>
+		</div>
+	{:else}
+		<h1>Campaign not found.</h1>
+	{/if}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+
+	div {
+		align-self: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 24px 0;
+	}
+
+	.name {
+		font-weight: 400;
+		margin: 12px 0 6px 0;
+	}
+
+	.name,
+	.description {
+		text-align: left;
+	}
+</style>
--- src/routes/campaigns/[slug]/+page.ts
+++ src/routes/campaigns/[slug]/+page.ts
@@ -1,10 +1,5 @@
-import { error } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
- 
 export function load({ params }) {
-    console.log(params.slug);
     return {
-        title: 'Hello world!',
-        content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+        campaignId: params.slug,
     };
 }
