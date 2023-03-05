ae1ddaa5b56394916c69e3e1b09488b1f33fc1ff
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-05 12:30:36 +0100

    ✨ add primary prop on <Link>

--- src/lib/cdk/Link/Link.svelte
+++ src/lib/cdk/Link/Link.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	export let href: string;
+	export let primary: boolean = false;
 </script>
 
-<a {href} class="button-shaped-round mdc-button mdc-top-app-bar__action-item mdc-ripple-upgraded">
+<a
+	{href}
+	class="button-shaped-round mdc-button mdc-top-app-bar__action-item mdc-ripple-upgraded"
+	class:mdc-button--unelevated={primary}
+>
 	<div class="mdc-button__ripple" />
 	<span class="mdc-button__label"> <slot /></span></a
 >
