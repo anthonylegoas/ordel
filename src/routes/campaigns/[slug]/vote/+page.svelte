b12216a46724b755bd25e6802809e3613b59a3c6
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-08 18:00:57 +0100

    ✨ add vote page

---
+++ src/routes/campaigns/[slug]/vote/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+	import { Vote } from '$lib/components';
+	import { campaignsStore } from '$lib/store';
+	type CampaignPageData = { campaignId: string } & PageData;
+
+	export let data: CampaignPageData;
+	const { campaigns } = campaignsStore;
+	const campaign = data.campaignId ? $campaigns.find((v) => v.id === data.campaignId) : null;
+</script>
+
+<section>
+	{#if campaign}
+		<h1 class="name">{campaign.name}</h1>
+		<p class="description">{campaign.description}</p>
+		<Vote {campaign} />
+		<p>Click on the proposal you wish to retain (or use its keyboard shortcut)</p>
+	{/if}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+
+	.name {
+		margin: 12px 0 6px 0;
+	}
+
+	.name,
+	.description {
+		text-align: left;
+	}
+</style>
