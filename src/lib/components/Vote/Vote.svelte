57c43c3e36702c4f087593c90949b36412019415
Anthony Le Goas <anthony.legoas@gmail.com>
2023-03-08 18:00:22 +0100

    ✨ add <Vote>

---
+++ src/lib/components/Vote/Vote.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import VoteCard from './VoteCard/VoteCard.svelte';
+	import type { Campaign } from '$lib/store';
+
+	export let campaign: Campaign;
+	const proposals = campaign.objects;
+</script>
+
+<div>
+	{#each proposals as proposal, i}
+		<VoteCard imageUrl={proposal.imageUrl} title={`Proposal ${i + 1}`} />
+	{/each}
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+		width: 100%;
+	}
+</style>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export * from './OrdelInstructions'
+export * from './Vote'
