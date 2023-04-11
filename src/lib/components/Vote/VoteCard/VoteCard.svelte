ce8501c062ae85703ee5e19dda0a85cb85ab0327
Anthony Le Goas <anthony.legoas@gmail.com>
2023-04-11 21:22:47 +0200

    💫 add animation when a proposal is selected

--- src/lib/components/Vote/VoteCard/VoteCard.svelte
+++ src/lib/components/Vote/VoteCard/VoteCard.svelte
@@ -4,39 +4,55 @@
 	import Button, { Label } from '@smui/button';
 	import VoteCardZoomDialog from './VoteCardZoomDialog.svelte';
 	import { createEventDispatcher } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
 	import type { CampaignObject } from '$lib/store';
 
 	export let proposal: CampaignObject;
 	export let proposalIndex: number;
 	let zoomDialogOpened = false;
 
+	let selected = false;
 	const dispatch = createEventDispatcher();
-	const cardSelectedHandler = () => dispatch('proposalVoted', { proposal });
+	const cardSelectedHandler = () => {
+		selected = true;
+		setTimeout(() => {
+			selected = false;
+		}, duration);
+		dispatch('proposalVoted', { proposal });
+	};
 	const cardActionsZoomHandler = () => (zoomDialogOpened = true);
 	const closeModalHandler = () => (zoomDialogOpened = false);
+	const duration = 450;
+	$: move = selected ? (proposalIndex === 1 ? -200 : 200) : 0;
 </script>
 
-<Card style="min-width: 42%; padding: 18px 18px 0px 18px;">
-	<PrimaryAction on:click={cardSelectedHandler} class="voteImg">
-		<Media
-			class="card-media-16x9"
-			aspectRatio="16x9"
-			style={`background-image: url(${proposal.imageUrl});`}
-		/>
-		<div class="voteBtn">
-			<Button on:click={cardSelectedHandler} variant="unelevated" class="button-shaped-round">
-				<Label>Choose this one</Label>
-			</Button>
-		</div>
-	</PrimaryAction>
-	<Actions>
-		<ActionIcons>
-			<IconButton class="material-icons" on:click={cardActionsZoomHandler} title="Zoom"
-				><span class="material-symbols-outlined"> zoom_out_map </span></IconButton
-			>
-		</ActionIcons>
-	</Actions>
-</Card>
+<div
+	style="min-width: 42%; padding: 18px 18px 0px 18px;"
+	in:fade={{ duration }}
+	out:fly={{ duration, x: move }}
+>
+	<Card style="padding: 18px 18px 0px 18px;">
+		<PrimaryAction on:click={cardSelectedHandler} class="voteImg">
+			<Media
+				class="card-media-16x9"
+				aspectRatio="16x9"
+				style={`background-image: url(${proposal.imageUrl});`}
+			/>
+			<div class="voteBtn">
+				<Button on:click={cardSelectedHandler} variant="unelevated" class="button-shaped-round">
+					<Label>Choose this one</Label>
+				</Button>
+			</div>
+		</PrimaryAction>
+		<Actions>
+			<ActionIcons>
+				<IconButton class="material-icons" on:click={cardActionsZoomHandler} title="Zoom"
+					><span class="material-symbols-outlined"> zoom_out_map </span></IconButton
+				>
+			</ActionIcons>
+		</Actions>
+	</Card>
+</div>
 
 <VoteCardZoomDialog
 	imageUrl={proposal.imageUrl}
