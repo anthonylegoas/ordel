33786d8b98865ffd4f82049fe99ebb03a6621578
Anthony Le Goas <anthony.legoas@gmail.com>
2023-04-12 18:26:35 +0200

    ✨ add <CampaignStatsParticipation>

---
+++ src/lib/components/Campaigns/CampaignsList/CampaignsStats/CampaignStatsParticipation.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Campaign } from '$lib/store';
+	import { Section as OrdelSection } from '$lib/cdk';
+
+	export let campaign: Campaign;
+</script>
+
+<OrdelSection title="Participation" subtitle="1500+ displays">
+	<p>to do</p>
+</OrdelSection>
---
+++ src/lib/components/Campaigns/CampaignsList/CampaignsStats/index.ts
@@ -0,0 +1,1 @@
+export { default as CampaignStatsParticipation } from './CampaignStatsParticipation.svelte'
