1b296cccd7bc20f15b058a64599ac2951b812a06
Anthony Le Goas <anthony.legoas@gmail.com>
2023-04-17 19:39:56 +0200

    ✨ add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<title>Error</title>
+	<meta name="description" content="Ordel - error" />
+</svelte:head>
+<div>
+	<h1>New World Ordel</h1>
+	<p>404</p>
+</div>
+
+<style>
+	div {
+		margin: 150px 0;
+	}
+	h1,
+	p {
+		font-family: 'Source Serif Pro';
+		font-style: normal;
+		font-weight: 900;
+		font-size: 57px;
+		line-height: 64px;
+		margin: 0;
+		text-align: center;
+		color: var(--mdc-theme-primary);
+	}
+</style>
